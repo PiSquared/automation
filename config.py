e7f7ab03c2e00265f5de1567a6d8caaf9a5e4567
Mark Murnane <mark@hackafe.net>
2019-08-26 18:51:39 -0400

    Initial Commit

---
+++ config.py
@@ -0,0 +1,5 @@
+SERIAL_PORT = "COM3"
+SERIAL_BAUDRATE = 19200
+
+APP_DIR = "F:\\Applications"
+APP_LOCK = "F:\\Applications\\app.lock"
---
+++ start.bat
@@ -0,0 +1,1 @@
+start /min "Control Server" flask run --host=0.0.0.0
